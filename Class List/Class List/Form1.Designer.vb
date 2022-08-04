d7411c83684201bfc41c30d7a0d51d008f99b624
Monde Castro <45668982+Monde-Castro@users.noreply.github.com>
2022-08-04 16:35:59 +0200

    Adding files via upload

---
+++ Class List/Class List/Form1.Designer.vb
@@ -0,0 +1,192 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmClassList
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtAmount = New System.Windows.Forms.TextBox()
+        Me.btnAmount = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Enter = New System.Windows.Forms.Button()
+        Me.txtSurname = New System.Windows.Forms.TextBox()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.lstClassList = New System.Windows.Forms.ListBox()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(33, 38)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(171, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Amount Student:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(6, 41)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(135, 24)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Student Name:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(6, 75)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(161, 24)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Student Surname:"
+        '
+        'txtAmount
+        '
+        Me.txtAmount.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtAmount.Location = New System.Drawing.Point(210, 36)
+        Me.txtAmount.Name = "txtAmount"
+        Me.txtAmount.Size = New System.Drawing.Size(148, 29)
+        Me.txtAmount.TabIndex = 3
+        '
+        'btnAmount
+        '
+        Me.btnAmount.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAmount.Location = New System.Drawing.Point(87, 83)
+        Me.btnAmount.Name = "btnAmount"
+        Me.btnAmount.Size = New System.Drawing.Size(241, 32)
+        Me.btnAmount.TabIndex = 4
+        Me.btnAmount.Text = "Enter &Amount"
+        Me.btnAmount.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Enter)
+        Me.GroupBox1.Controls.Add(Me.txtSurname)
+        Me.GroupBox1.Controls.Add(Me.txtName)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(38, 159)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(339, 160)
+        Me.GroupBox1.TabIndex = 5
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Student Detail"
+        '
+        'Enter
+        '
+        Me.Enter.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Enter.Location = New System.Drawing.Point(35, 121)
+        Me.Enter.Name = "Enter"
+        Me.Enter.Size = New System.Drawing.Size(241, 33)
+        Me.Enter.TabIndex = 6
+        Me.Enter.Text = "Enter &Student"
+        Me.Enter.UseVisualStyleBackColor = True
+        '
+        'txtSurname
+        '
+        Me.txtSurname.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtSurname.Location = New System.Drawing.Point(188, 75)
+        Me.txtSurname.Name = "txtSurname"
+        Me.txtSurname.Size = New System.Drawing.Size(132, 29)
+        Me.txtSurname.TabIndex = 7
+        '
+        'txtName
+        '
+        Me.txtName.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtName.Location = New System.Drawing.Point(188, 39)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(132, 29)
+        Me.txtName.TabIndex = 6
+        '
+        'lstClassList
+        '
+        Me.lstClassList.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstClassList.FormattingEnabled = True
+        Me.lstClassList.ItemHeight = 24
+        Me.lstClassList.Location = New System.Drawing.Point(398, 25)
+        Me.lstClassList.Name = "lstClassList"
+        Me.lstClassList.Size = New System.Drawing.Size(253, 268)
+        Me.lstClassList.TabIndex = 6
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(398, 337)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 33)
+        Me.btnClear.TabIndex = 8
+        Me.btnClear.Text = "&Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(576, 337)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 33)
+        Me.btnExit.TabIndex = 9
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'frmClassList
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(673, 382)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.lstClassList)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.btnAmount)
+        Me.Controls.Add(Me.txtAmount)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmClassList"
+        Me.Text = "Class List"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtAmount As System.Windows.Forms.TextBox
+    Friend WithEvents btnAmount As System.Windows.Forms.Button
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Enter As System.Windows.Forms.Button
+    Friend WithEvents txtSurname As System.Windows.Forms.TextBox
+    Friend WithEvents txtName As System.Windows.Forms.TextBox
+    Friend WithEvents lstClassList As System.Windows.Forms.ListBox
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+
+End Class
---
+++ Class List/Class List/Form1.vb
@@ -0,0 +1,71 @@
+﻿Public Class frmClassList
+
+
+    Public Counter As Integer
+    Public Counter2 As Integer
+    Public AmountArray() As String
+
+    Private Sub frmClassList_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Prevent Erro if arrays empty
+        GroupBox1.Enabled = False
+    End Sub
+  
+    Private Sub btnAmount_Click(sender As Object, e As EventArgs) Handles btnAmount.Click
+        'Setup the Amount of student in the class
+        Counter = CInt(txtAmount.Text) - 1
+
+        ReDim AmountArray(Counter)
+
+        If txtAmount.Text > 0 Then
+            GroupBox1.Enabled = True
+        End If
+
+        txtAmount.Clear()
+
+    End Sub
+
+    Private Sub Enter_Click(sender As Object, e As EventArgs) Handles Enter.Click
+        'Enter Student into Array
+        Dim K As Integer
+
+
+        AmountArray(Counter2) = txtName.Text & "  " & txtSurname.Text
+
+        lstClassList.Sorted = True
+        txtName.Focus()
+        txtName.Clear()
+        txtSurname.Clear()
+
+        If Counter2 = Counter Then
+            MessageBox.Show("Limit was reached")
+            For K = 0 To Counter
+                lstClassList.Items.Add(AmountArray(K))
+            Next
+            Exit Sub
+        End If
+
+        Counter2 = Counter2 + 1
+
+    End Sub
+
+    Sub ClearTextBox()
+        'Procedure to clear textbox
+        txtName.Clear()
+        txtSurname.Clear()
+        txtAmount.Clear()
+        GroupBox1.Enabled = False
+        lstClassList.Items.Clear()
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        'Clear the textbox and listbox using an procedure
+        ClearTextBox()
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'Close the program
+        Me.Close()
+    End Sub
+End Class
